562fb760b2e308b4a881cc515dac28b07096d58c
Hoang <hoang.viet.nguyen@outlook.com>
2021-09-08 19:21:16 -0400

    added scroll animation to Navbar

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useRef, useLayoutEffect, useState } from 'react';
+import styled, { css } from 'styled-components';
 import Navlinks from './Navlinks';
 
 import SectionInner from '../components/Section/SectionInner';
 import { Link } from 'react-scroll';
 
-const Container = styled.div`
+const activeStyles = css`
   background-color: ${({ theme }) => theme.clr.primary[100]};
+`;
+
+const Container = styled.div`
+  background-color: ${({ theme }) => theme.clr.neutral[200]};
   position: sticky;
   width: 100%;
   z-index: 100;
   top: 0;
+  height: ${({ theme }) => theme.navbarHeight};
+  ${(props) => props.$active && activeStyles};
+
+  transition: 0.4s ease-out;
 `;
 
 const Inner = styled(SectionInner)`
@@ -18,7 +26,7 @@ const Inner = styled(SectionInner)`
 
   align-items: center;
   justify-content: space-between;
-  height: ${({ theme }) => theme.navbarHeight};
+  height: 100%;
   max-width: ${({ theme }) => theme.maxWidth};
 `;
 
@@ -35,8 +43,27 @@ const Logo = styled(Link).attrs((props) => ({
 `;
 
 const Navbar = () => {
+  const navRef = useRef();
+  const [active, setActive] = useState();
+
+  console.log(active);
+  const onScroll = () => {
+    if (window.scrollY > navRef.current.offsetHeight + 50) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
   return (
-    <Container>
+    <Container ref={navRef} $active={active}>
       <Inner paddingy='0' gap='0'>
         <Logo>HN</Logo>
         <Navlinks />
